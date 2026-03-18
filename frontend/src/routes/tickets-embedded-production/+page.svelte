e4dfb0f7e515cc063d15cfef3a5c22b7ffed2231
Joppe Koers <joppe@joppekoers.nl>
2026-03-18 16:12:33 +0100

    Add tickets page

---
+++ frontend/src/routes/tickets-embedded-production/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<link rel="preconnect" href="https://api.celebratix.io" />
+	<link rel="preconnect" href="https://img.celebratix.io" />
+	<link rel="stylesheet" href="https://shop.celebratix.io/celebratix-style.css" />
+	<script src="https://shop.celebratix.io/celebratix-index.js" type="module"></script>
+</svelte:head>
+
+<div id="celebratix-ticket-widget" data-tos="" data-channel="joppe" />
---
+++ frontend/src/routes/tickets-iframe-staging/+page.svelte
@@ -0,0 +1,6 @@
+<iframe
+	src="https://shop.staging.celebratix.io?c=joppe"
+	width="1000"
+	height="1000"
+	id="celebratix-ticket-shop"
+	title="Ticket Shop" />
